36408edaafb4d3b9c7cb26fdd6c40bc9d7400142
Sergey Stelmakh <stelmakhserg@gmail.com>
2017-04-24 21:11:29 +0300

    added router module & navbar component template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import {DatepickerModule} from 'ngx-bootstrap/datepicker';
+
+//routing
+import {routing} from "./app.routing";
 
 import { AppComponent } from './app.component';
+import {NavbarComponent} from './components/navbar/navbar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    DatepickerModule.forRoot()
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routing.ts
@@ -0,0 +1,16 @@
+/**
+ * Created by SergST on 24.04.2017.
+ */
+
+import {Routes, RouterModule} from "@angular/router";
+
+
+const appRoutes: Routes = [
+  // { path: '', redirectTo: 'home', pathMatch: 'full'},
+  // { path: 'users', component: UserListComponent },
+  // { path: 'schedules', component: ScheduleListComponent },
+  // { path: 'schedules/:id/edit', component: ScheduleEditComponent },
+  // { path: 'home', component: HomeComponent }
+];
+
+export const routing = RouterModule.forRoot(appRoutes);
